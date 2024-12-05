50187991439ccb2a5ff30b670da557d0f14b2364
Carlos Dávila <69244347+davila795@users.noreply.github.com>
2024-12-05 21:48:49 +0100

    Questionnaire with description implemented. EmotionsSlice modified to match the new type structure including the description.

--- client/src/features/api/questionnaireApi.ts
+++ client/src/features/api/questionnaireApi.ts
@@ -1,17 +1,27 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {EmotionsResponse} from "../../types/emotions.ts";
 import {Questionnaire, SubmitResponseItem} from "../../types/questionnaire.ts";
+import {RootState} from "../../app/store.ts";
 
 export const questionnaireApi = createApi({
   reducerPath: 'questionnaireApi',
-  baseQuery: fetchBaseQuery({baseUrl: 'https://localhost:7116/api'}), // Adjust base URL as needed
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://localhost:7116/api',
+    prepareHeaders: (headers, {getState}) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }), // Adjust base URL as needed
   endpoints: (builder) => ({
     fetchQuestions: builder.query<Questionnaire, void>({
-      query: () => '/questionary', // API endpoint for fetching questions
+      query: () => '/Questionary', // API endpoint for fetching questions
     }),
     submitResponses: builder.mutation<EmotionsResponse, SubmitResponseItem[]>({
       query: (body) => ({
-        url: 'questionary',
+        url: 'Questionary',
         method: 'POST',
         body,
       })
